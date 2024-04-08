637e0f8320cc667cf21401866ef244284ad21add
esraa <esraa.masuad@gmail.com>
2024-04-08 11:10:04 +0200

    Initial Commit

---
+++ OrderApp/OrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppApp.swift
+//  OrderApp
+//
+//  Created by Esraa Gomaa on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
